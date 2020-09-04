47eeb0ca211259462e68b607565be03fdd42fe7a
khyatish <khyatishjhala@gmail.com>
2020-09-04 16:09:46 +0530

    Create simple new user from

---
+++ newuser.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Entry</title>
+</head>
+<body>
+    <div class='container'>
+      <table class="table">
+        <tr>
+            <td>
+                <label class="card-title">Name</label>
+                <input class="form-control" type="text" name="txtusername" placeholder="Your Name">
+            </td>
+        </tr>
+        
+        <tr>
+            <td>
+                <label class="card-title">Phone Number</label>
+                <input class="form-control" type="text" name="txtmobile" placeholder="Your Phone Number">
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <label class="card-title">Email</label>
+                <input class="form-control" type="text" name="txtmail" placeholder="Your Name">
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <label class="card-title">Password</label>
+                <input class="form-control" type="text" name="txtpassword" placeholder="Your Password">
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <button type="submit" class="btn btn-outline-primary" name="makenew">Register your Details</button>
+            </td>
+        </tr>
+
+      </table>
+    </div>  
+</body>
+</html>
