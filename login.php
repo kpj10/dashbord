73b17d399ac3a7a3530ca0239bcff0beac3daa9c
khyatish <khyatishjhala@gmail.com>
2020-09-04 16:08:01 +0530

    Convert old Login Form to Responsive using css and bootstrap freamwork

--- login.php
+++ login.php
@@ -12,33 +12,54 @@ if (isset($_POST['submit_data']))
 <!DOCTYPE html>
 <html lang="en">
     <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <title>Login Form</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/style.css" rel="stylesheet">
+        
     </head>
-    <body>
-        <form method="POST">
-            <table align='center' border='1'>
-            <tr>
-                <td colspan='2' align='center'>Login Form</td>
-            </tr>
+    <body background='p1.jpeg'>
+   <!-- <img src="p1.jpeg" class="img-fluid" alt="Responsive image"> -->
+    
+    <form method="POST">
+        <div class="container">  
+            <div class="card" style="width: 250px;">
+                <div class="card-header">
+                    Welcome to World
+                </div>
 
-            <tr>
-                <td>username</td>
-                <td><input type="text" name="txtusername"></td>
-            </tr>
-
-            <tr>
-                <td>Password</td>
-                <td><input type="password" name="txtpassword"></td>
-            </tr>
-
-            <tr>
-                <td colspan='2' align='center'><button type="submit" name="submit_data">Login</button></td>
-            </tr>
-            </table>
-        </form>   
+                <div class="body">
+                <table align='center'>
+                
+                    <div class="form-group">
+                        <tr>
+                            <td>
+                                <label class="card-title">Email</label>
+                                <input class="form-control" type="text" name="txtusername" placeholder="Your Email">
+                            </td>
+                        </tr>
+                         
+                        <tr>
+                            <td>
+                                <label class="card-title">Password</label>
+                                <input class="form-control" type="password" name="txtpassword">
+                            </td>
+                        </tr>
+                            
+                        <tr>
+                            <td>
+                                <div name="btn">
+                                <button class="btn btn-primary" type="submit" name="submit_data">Login to Explor</button>        
+                                </div>
+                            </td></tr>
+                        
+                    </div>
+                </table>    
+                </div>
+            </div>
+       </div>     
+    </form>     
     </body>
 </html>
 
