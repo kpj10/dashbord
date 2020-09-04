45e2a9ed4c9108c4eeedd15aa283bdacae15d202
khyatish <khyatishjhala@gmail.com>
2020-09-04 18:04:37 +0530

    Add Php to insert data from the database

--- newuser.php
+++ newuser.php
@@ -1,3 +1,29 @@
+<?php
+include 'config.php';
+
+if (isset($_POST['makenew'])) 
+{    
+    $username=$_POST['txtusername'];
+    $phone_no=$_POST['txtmobile'];
+    $mail=$_POST['txtmail'];
+    $password=$_POST['txtpassword'];
+
+    $sql = "insert into newuser(name,number,email,password) values('".$username."','".$phone_no."','".$mail."','".$password."')";
+
+    $add_data=execute($con,$sql);
+
+    if ($add_data) 
+    {
+        echo "<script>alert('Your Data Register Successfully');</script>";
+        //header('location:login.php');
+    } else {
+        echo "<script>alert('I See Some Error');</script>";
+    }
+    
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,10 +32,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Entry</title>
+
+    <style>
+        #tab_border{
+            border-style: outset; 
+        }
+        
+        body {background-color: #C2FBFF;}
+    </style>
+    
 </head>
 <body>
+    <form method="POST">
     <div class='container'>
-      <table class="table">
+    <div class="row">
+
+<div class="col-sm"></div>
+
+<div class="col-sm" id="tab_border">    
+    <table class="table">
         <tr>
             <td>
                 <label class="card-title">Name</label>
@@ -34,7 +75,7 @@
         <tr>
             <td>
                 <label class="card-title">Password</label>
-                <input class="form-control" type="text" name="txtpassword" placeholder="Your Password">
+                <input class="form-control" type="password" name="txtpassword" placeholder="Your Password">
             </td>
         </tr>
 
@@ -44,7 +85,13 @@
             </td>
         </tr>
 
-      </table>
+    </table>
+</div>
+
+<div class="col-sm"></div>
+      
     </div>  
+    </div>  
+    </form>
 </body>
 </html>
